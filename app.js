933ebc3da66eb83bb4e0165f45a653bc25fcdc42
mic40472 <mic40472@gmail.com>
2025-05-06 11:53:36 +0000

    updated timer app code and add replica in deployment yaml

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
+let startTime = Date.now();
+
 const server = http.createServer((req, res) => {
-  res.end('Hello from GCP Kubernetes!-1');
+res.end('Hello from GCP Kubernetes!-1');
 });
+
+// Timer that logs uptime every 1 second
+setInterval(() => {
+const elapsedMs = Date.now() - startTime;
+const seconds = Math.floor(elapsedMs / 1000) % 60;
+const minutes = Math.floor(elapsedMs / 1000 / 60);
+console.log(Server uptime: ${minutes}m ${seconds}s);
+}, 1000);
 
 server.listen(8080, () => {
-  console.log('Server running on port 8080');
+console.log('Server running on port 8080');
 });
-
